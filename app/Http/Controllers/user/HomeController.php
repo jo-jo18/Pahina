0d1d152e48afab5d115ae5f7cb336dae61a834ae
jo-jo <carbonillajoelen@gmail.com>
2026-03-15 16:14:07 +0800

    ti isa isahon tani?

--- app/Http/Controllers/user/HomeController.php
+++ app/Http/Controllers/user/HomeController.php
@@ -7,20 +7,17 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Show home section.
-     */
+    
     public function index()
     {
         return view('user.home');
     }
 
-    /**
-     * Get featured books (API).
-     */
+    
     public function getFeatured()
     {
-        // TODO: return featured books
+        
         return response()->json([]);
+        
     }
 }
