de99d8ca6c9c9042d9ac845839d2e6b5060e4b7f
peronalim123 <rperonalim5@gmail.com>
2026-04-12 09:03:29 -0700

    Fix merge conflicts

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,4 +1,7 @@
-protected $routeMiddleware = [
-    // ...
-    'admin' => \App\Http\Middleware\AdminMiddleware::class,
+protected $middlewareGroups = [
+    'api' => [
+        \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        'throttle:api',
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    ],
 ];
